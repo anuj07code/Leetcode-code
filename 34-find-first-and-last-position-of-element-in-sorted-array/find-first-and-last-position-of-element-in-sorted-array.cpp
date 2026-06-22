1ef29a0cde0687134534cc6d39bf90dd8cf4dad3
anuj07code <anujpatwa30@gmail.com>
2026-06-23 01:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (53.41%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,54 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-       int n=nums.size();
-       int first=-1;
-       int last=-1;
-       for(int i=0;i<n;i++){
-           if(nums[i]==target){
-            if(first==-1){
-                first=i;
+    int lowerbound(vector<int>& nums,int n,int target){
+        int low=0,high=n-1,ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            } else {
+                low=mid+1;
             }
-            last=i;
-           }
-       }
-       return {first,last};
+        }
+        return ans;
     }
+
+    int upperbound(vector<int>& nums,int n,int target){
+        int low=0,high=n-1,ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>target){
+                ans=mid;
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int lb = lowerbound(nums,n,target);
+        if(lb==n || nums[lb]!=target) return {-1,-1};
+        int ub = upperbound(nums,n,target);
+        return {lb, ub-1};
+    }
+
+    // vector<int> searchRange(vector<int>& nums, int target) {
+    //    int n=nums.size();
+
+    //    int first=-1;
+    //    int last=-1;
+    //    for(int i=0;i<n;i++){
+    //        if(nums[i]==target){
+    //         if(first==-1){
+    //             first=i;
+    //         }
+    //         last=i;
+    //        }
+    //    }
+    //    return {first,last};
+    // }
 };
